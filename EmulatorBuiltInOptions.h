0dcd0b84f94551f4783b9b807bad5e2beaad0d9c
LMauricius <mauriciosmit100@gmail.com>
2025-08-21 00:52:52 +0200

    Formatting

--- EmulatorBuiltInOptions.h
+++ EmulatorBuiltInOptions.h
@@ -1,39 +1,30 @@
 #ifndef EMULATORBUILTINOPTIONS_H
 #define EMULATORBUILTINOPTIONS_H
 
-#include <map>
-#include <list>
 #include <QString>
+#include <list>
+#include <map>
 
 std::map<QString, std::list<std::pair<QString, QString>>> OptionsPerProgram = {
-    {"epsxe", {
-         {"Run from CD", " -cdrom (DRIVE)"},
-         {"Show PS splashscreen", " -slowboot"},
-         {"Start in fullscreen", " -fullscreen"},
-         {"Run without a GUI", " -nogui"}
-     }
-    },
-    {"pcsx2", {
-         {"Run from CD/DVD", " --usecd"},
-         {"Show PS2 splashscreen (not recommended - game doesn't boot)", " --fullboot"},
-         {"Start in fullscreen", " --fullscreen"},
-         {"Run without a GUI", " --nogui"}
-     }
-    },
-    {"ppsspp", {
-         {"Start in fullscreen", " --fullscreen"}
-     }
-    },
-    {"dolphin", {
-         // Windows version requires a ':' at end of drive to recognize it as a drive
+    {"epsxe",
+     {{"Run from CD", " -cdrom (DRIVE)"},
+      {"Show PS splashscreen", " -slowboot"},
+      {"Start in fullscreen", " -fullscreen"},
+      {"Run without a GUI", " -nogui"}}},
+    {"pcsx2",
+     {{"Run from CD/DVD", " --usecd"},
+      {"Show PS2 splashscreen (not recommended - game doesn't boot)", " --fullboot"},
+      {"Start in fullscreen", " --fullscreen"},
+      {"Run without a GUI", " --nogui"}}},
+    {"ppsspp", {{"Start in fullscreen", " --fullscreen"}}},
+    {"dolphin",
+     {
+// Windows version requires a ':' at end of drive to recognize it as a drive
 #ifdef _WIN32
          {"Run from CD/DVD", " --exec=(DRIVE):"},
 #elif defined __unix__
          {"Run from CD/DVD", " --exec=(DRIVE)"},
 #endif
-         {"Skip game list (open quickly)", " --batch"}
-     }
-    }
-};
+         {"Skip game list (open quickly)", " --batch"}}}};
 
 #endif // EMULATORBUILTINOPTIONS_H
--- main.cpp
+++ main.cpp
@@ -11,68 +11,60 @@
 #include <QtWidgets/QApplication>
 #include <qlogging.h>
 
-
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-	app.setApplicationName("EmuDiscer");
+    app.setApplicationName("EmuDiscer");
     app.setApplicationVersion(EMUDISCER_APP_VERSION);
 
-	// command line
-	QCommandLineParser parser;
-	parser.addHelpOption();
-	parser.addVersionOption();
+    // command line
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
 
-	QCommandLineOption settingsOption(
-		{ "s", "open-settings" },
-		"Open settings window at launch"
-	);
-	parser.addOption(settingsOption);
-    QCommandLineOption multiInstanceOption(
-        { "m", "multi-instance" },
-        "Run program even if another instance is running"
-    );
+    QCommandLineOption settingsOption({"s", "open-settings"}, "Open settings window at launch");
+    parser.addOption(settingsOption);
+    QCommandLineOption multiInstanceOption({"m", "multi-instance"},
+                                           "Run program even if another instance is running");
     parser.addOption(multiInstanceOption);
 
-	parser.process(app);
+    parser.process(app);
 
     // single instance
     QSharedMemory sharedMemory;
-    sharedMemory.setKey (QString("EmuDiscerSharedData_")+getUsername());
+    sharedMemory.setKey(QString("EmuDiscerSharedData_") + getUsername());
     qDebug() << sharedMemory.nativeKey() << "\n";
-    if (!sharedMemory.create(sizeof(SharedMemData)) &&
-        !parser.isSet(multiInstanceOption)) {
-      qDebug() << "Already running" << "\n";
-      if (sharedMemory.attach()) {
-        sharedMemory.lock();
-        ((SharedMemData *)sharedMemory.data())->raiseWindow = true;
-        sharedMemory.unlock();
+    if (!sharedMemory.create(sizeof(SharedMemData)) && !parser.isSet(multiInstanceOption)) {
+        qDebug() << "Already running" << "\n";
+        if (sharedMemory.attach()) {
+            sharedMemory.lock();
+            ((SharedMemData *)sharedMemory.data())->raiseWindow = true;
+            sharedMemory.unlock();
 
-        sharedMemory.detach();
-        return 0;
-      } else {
-        qDebug()
-            << "Couldn't use shared memory to notify the existing instance!"
-            << "\n";
-      }
+            sharedMemory.detach();
+            return 0;
+        } else {
+            qDebug() << "Couldn't use shared memory to notify the existing instance!"
+                     << "\n";
+        }
     }
 
     // options window
     EmuDiscer w(&sharedMemory);
 
-	if (parser.isSet(settingsOption))
-		w.show();
+    if (parser.isSet(settingsOption))
+        w.show();
 
     // attach to shared data
-        if (sharedMemory.attach()) {
+    if (sharedMemory.attach()) {
 
-          sharedMemory.lock();
-          ((SharedMemData *)sharedMemory.data())->raiseWindow = false;
-          sharedMemory.unlock();
-        } else {
-          qDebug() << "Couldn't setup shared memory\n";
-        }
+        sharedMemory.lock();
+        ((SharedMemData *)sharedMemory.data())->raiseWindow = false;
+        sharedMemory.unlock();
+    } else {
+        qDebug() << "Couldn't setup shared memory\n";
+    }
 
-        // finally, execute qt app
-        return app.exec();
+    // finally, execute qt app
+    return app.exec();
 }
